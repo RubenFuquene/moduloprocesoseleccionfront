f5efcfaee0cc7c729558613a95ac1706e9ad9b80
RubenFuquene <rubendariofuquene@gmailcom>
2024-05-23 20:01:12 -0500

    Register Module

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+// src/types/index.ts
+export interface CargoType {
+  IDTIPOCARGO: string;
+  DESCTIPOCARGO: string;
+}
